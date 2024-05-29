55a69a7764f6c316ca87d6541fb6975a9eafd973
padriana <adriana.pullig@ynov.com>
2024-05-29 09:34:35 +0200

    instaal

--- Serveur/installationDotNet.sh
+++ Serveur/installationDotNet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update && apt upgrade -y && apt-get install -y wget &&apt-get install -y sudo
+apt-get update && apt-get upgrade -y && apt-get install -y wget && apt-get install -y sudo
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
